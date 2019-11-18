7793f479a3daf7c3923815cd86708e0fd3822e92
Viðar Vignisson <vidar0175@easv365.dk>
2019-11-18 23:50:38 +0100

    initial commit

---
+++ gremblo_grace.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Plugin Name: Gremblo Images
+ * Description: With this plugin you can see a fresh image of our lord and saviour Gremblo every day!
+ * Version: 0.1
+ * Author: Viðar Vignisson
+ * Author URL: https://prep4this.com
+ * Text Domain: 
+*/
+
+if ( ! function_exists( 'add_action')) {
+    echo "You being here is in direct violation of our lord Gremblos' will, leave or die!";
+    exit;
+}
+
+class avatarOfGremblo {
+    function __construct()
+    {
+        add_action('wp_dashboard_setup', array($this, 'my_custom_dashboard_widgets'));
+        add_action( 'admin_init', array($this, 'styling'));
+    }
+
+    function my_custom_dashboard_widgets() {
+        global $wp_meta_boxes;
+         
+        wp_add_dashboard_widget('gremblo_dashboard', 'Gremblo Image of the Day', array($this, 'gremblo_dashboard'));
+    }
+
+    function gremblo_dashboard() {
+        $data = file_get_contents (esc_url('http://grembloapi.test/api/images'));
+        $image = json_decode($data);
+
+        if ($image == true) 
+        {          
+            // style="background-image: url(\'' . $image->data->image_link . '\')"
+            echo '<div class="gremblo-widget"><img src="' . $image->data->image_link . '"></div>';
+        }
+        else {
+            echo '<div class="gremblo-widget"></div>';
+        }
+        
+    }
+
+    function styling(){
+        wp_enqueue_style( 'grembloStyle', plugins_url('gremblo.css', __FILE__ ));
+    }
+}
+
+if (class_exists('avatarOfGremblo')) {
+    $grembloCreate = new avatarOfGremblo();
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+//Silence is golden
+
+if ( ! function_exists( 'add_action')) {
+    echo "You being here is in direct violation of our lord Gremblos' will, leave or die!";
+    exit;
+}
